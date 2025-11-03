2db2d4b208e5759ab81d7a8fc44d1fc218792073
shraddhaopsmx <shraddha.singh@opsmx.io>
2025-11-03 18:44:24 +0530

    DB engine arg addition

--- src/ai_guardian_remediation/storage/db/db.py
+++ src/ai_guardian_remediation/storage/db/db.py
@@ -4,9 +4,21 @@ from sqlalchemy import create_engine, Engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from ai_guardian_remediation.config import settings
 
+# TODO: Delete later
+# --- Setup logging for SQLAlchemy pool ---
+logging.basicConfig(
+    level=logging.DEBUG,  # prints all SQLAlchemy debug logs
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+)
+logging.getLogger("sqlalchemy.pool").setLevel(logging.DEBUG)  # pool events
+
 Base = declarative_base()
 
-engine = create_engine(settings.DATABASE_URL)
+engine = create_engine(
+    settings.DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=1800,
+)
 Session = sessionmaker(bind=engine)
 # session = Session()
 
